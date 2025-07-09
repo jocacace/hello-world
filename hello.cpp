e6964b45f6d24c794faf858dccbbc4bbbf136cf2
Jonathan Cacace <myrobotmail1312@gmail.com>
2025-07-09 15:35:07 +0200

    new welcome message

--- hello.cpp
+++ hello.cpp
@@ -3,5 +3,7 @@
 
 int main()  {
 	std::cout << "Hello World, Welcome, Benvenuto, Bienvenido, Benvingut" << std::endl;
-	std::cout << "Hello also from collaborator" << std::endl;
+	std::cout << "We are happy that you are using thie program! Report any bug on the Issue of github!" << std::endl;
+
+
 }
