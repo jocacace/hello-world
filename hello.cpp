9c0a72d860a4c0af1f2d08a97f29cebd29b92ab6
Jonathan Cacace <myrobotmail1312@gmail.com>
2025-10-24 13:47:40 +0200

    new welcome messge

--- hello.cpp
+++ hello.cpp
@@ -3,6 +3,8 @@
 
 int main()  {
 	std::cout << "Hello World! Welcome, I am really happy to see u here!" << std::endl;
+	
+	std::cout << "Hello also from your collaborator!" << std::endl;
 
 
 	std::cout << "Good luck to forks!" << std::endl;
